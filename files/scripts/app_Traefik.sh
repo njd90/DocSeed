ae5cd23af53b55299932299f279b168ab809842d
NJD <njd@wanadoo.fr>
2018-04-10 20:31:26 +0200

    Correction wildcard

--- files/scripts/app_Traefik.sh
+++ files/scripts/app_Traefik.sh
@@ -4,12 +4,15 @@
 cat files/includes/traefik.docker >> docker-compose.yml
 
 mkdir -p $CONF_PATH/traefik
-cat files/includes/traefik.toml >> $CONF_PATH/traefik/traefik.toml
+cat files/includes/traefik.toml > $CONF_PATH/traefik/traefik.toml
+sed -i "s@domain.tld@$DOMAIN@g" $CONF_PATH/traefik/traefik.toml
+sed -i "s/EMAIL_CH/$MAIL/g" $CONF_PATH/traefik/traefik.toml
 touch $CONF_PATH/traefik/acme.json
+echo "{}" > $CONF_PATH/traefik/acme.json
 chmod 600 $CONF_PATH/traefik/acme.json
 
 sed -i "s/RFC2136_KEY/$RFC2136_KEY/g" docker-compose.yml
-sed -i "s/RFC2136_SECRET/$RFC2136_SECRET/g" docker-compose.yml
+sed -i "s@RFC2136_SECRET@$RFC2136_SECRET@g" docker-compose.yml
 sed -i "s/RFC2136_ALGO/$RFC2136_ALGO/g" docker-compose.yml
 sed -i "s/RFC2136_NS/$RFC2136_NS/g" docker-compose.yml
 sed -i "s/EMAIL_CH/$MAIL/g" docker-compose.yml
