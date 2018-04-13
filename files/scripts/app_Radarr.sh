eeaddcf83b3dfab3aeea5fbb3fb243cc2f164e3d
NJD <njd@wanadoo.fr>
2018-04-13 21:27:40 +0200

    Correction bug

--- files/scripts/app_Radarr.sh
+++ files/scripts/app_Radarr.sh
@@ -10,6 +10,7 @@ sed -i "s@FQDN@$Rd_SDOM.$DOMAIN@g" docker-compose.yml
 sed -i "s@INCOMING@$INC_PATH/movies@g" docker-compose.yml
 sed -i "s@RMOVIES@$MEDIA_PATH/movies@g" docker-compose.yml
 sed -i "s@autodl-movies_radarr@$Rd_CNAME@g" docker-compose.yml
+sed -i "s@HOME@$DEFAULT_PATH@g" docker-compose.yml
 
 # Set Muximux configuration
 cat <<EOF >> files/includes/muximux.conf
